71cbbc538a09475ff93e6bd939eb74383f48c220
garner1 <silvano.garnerone@gmail.com>
2020-01-30 16:34:13 +0100

    minor

--- SG/viz_module/see_tiff.sh
+++ SG/viz_module/see_tiff.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-img=$1  #iMS337_20190709_001.tif
-pyramid=$2  #iMS337.pyramid.tif
+tiffdir=$1 #dir containing pyramid tiff files
 
 mother=/usr/local/share/anaconda3/bin/  #path/to/anaconda/bins
 deep=/home/garner1/openslide-python/examples/deepzoom #path/to/deepzoom
 
-[[ -d ${mother} ]] || echo ${mother} does not exists!
-[[ -d ${deep} ]] || echo ${deep} does not exists!
-[[ -f ${img} ]] || echo ${img} does not exists!
-[[ -f ${pyramid} ]] || vips tiffsave ${img} ${pyramid} --tile --tile-width=256 --tile-height=256 --pyramid
-
+${mother}/python3 ${deep}/deepzoom_multiserver.py -Q 100 ${tiffdir}
