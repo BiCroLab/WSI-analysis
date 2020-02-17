c029831ad4563379d97928d66fdc6dc7b681e12b
garner1 <silvano.garnerone@gmail.com>
2020-02-17 09:36:24 +0100

    prepare pyramidal tif that can be read in qupath

---
+++ SG/prepare.TIF.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+file=$1 #input file
+
+convert ${file} -define tiff:tile-geometry=128x128 -depth 8 ptif:${file}.8bit.convert.tif
+~/tools/bioimageconvert/imgcnv -i ${file}.8bit.convert.tif -o ${file}.8bit.convert.imgcnv.tif -t TIFF -options compression none tiles 512 pyramid subdirs
