3b0b55451a98a786bdf0dc425fcf4ba815ff2bad
garner1 <silvano.garnerone@gmail.com>
2020-02-24 10:26:33 +0100

    minor

--- SG/heatmap_module/pipe4.walk.fromQuPath.py
+++ SG/heatmap_module/pipe4.walk.fromQuPath.py
@@ -18,29 +18,52 @@ W = sparse.load_npz(sys.argv[2]) #id...graph.npz
 degreefile = sys.argv[3] #id...degree.gz
 ccfile = sys.argv[4]  #id...cc.gz
 steps = int(sys.argv[5]) #number of steps of the random walker
+# feature = sys.argv[6] #one of area,perimeter,circularity,eccentricity,intensity,degree,cc
 
-morpho = np.loadtxt(morphology, delimiter="\t", skiprows=True, usecols=(7,9,12,13))   # area,perimeter,circularity,eccentricity,intensity
-degree_vec = np.loadtxt(degreefile, delimiter=" ").reshape((morpho.shape[0],1))
-cc_vec = np.loadtxt(ccfile, delimiter=" ").reshape((morpho.shape[0],1))
+# if feature == 'area':
+#     data = np.loadtxt(morphology, delimiter="\t", skiprows=True, usecols=(7,))#.reshape((W.shape[0],1))
+# if feature == 'perimeter':
+#     data = np.loadtxt(morphology, delimiter="\t", skiprows=True, usecols=(8,))#.reshape((W.shape[0],1))
+# if feature == 'circularity':
+#     data = np.loadtxt(morphology, delimiter="\t", skiprows=True, usecols=(9,))#.reshape((W.shape[0],1))
+# if feature == 'eccentricity':
+#     data = np.loadtxt(morphology, delimiter="\t", skiprows=True, usecols=(12,))#.reshape((W.shape[0],1))
+# if feature == 'intensity':
+#     data = np.loadtxt(morphology, delimiter="\t", skiprows=True, usecols=(13,))#.reshape((W.shape[0],1))
+# if feature == 'degree':
+#     data = np.loadtxt(degreefile, delimiter=" ")#.reshape((W.shape[0],1))
+# if feature == 'cc':
+#     data = np.loadtxt(ccfile, delimiter=" ")#.reshape((W.shape[0],1))
 
-data = np.hstack((morpho,degree_vec,cc_vec)) # stack morphology and topology
-
+morpho = np.loadtxt(morphology, delimiter="\t", skiprows=True, usecols=(7,8,9,12,13)).reshape((W.shape[0],5))
+degree_vec = np.loadtxt(degreefile, delimiter=" ").reshape((W.shape[0],1))
+cc_vec = np.loadtxt(ccfile, delimiter=" ").reshape((W.shape[0],1))
+data = np.hstack((morpho,degree_vec,cc_vec))
+print(data.shape)
 ##################
 # Should I use a row stochastic matrix or not? In principle is not necessary
 #SS = normalize(W, norm='l1', axis=1) #create the row-stochastic matrix 
 ################
 
-history = data
-for counter in range(steps):
-    newdata = W.dot(data) # !!!use weighted graph adj not the stochastic matrix for the moment!!!
-    history = np.dstack((history,newdata))
-#    print(history.shape)
+# history = np.zeros((data.shape[0],data.shape[1],steps+1))
+# history[:,:,0] = data
+# for counter in range(steps):
+#     print(morphology+str( counter))
+#     history[:,:,counter+1] = W.dot(history[:,:,counter])
 
-if history.shape[0] == W.shape[0] and history.shape[2] == steps+1:
-    print("Saving history...")
-    filename = str(morphology)+'.walkhistory'    
-    np.save(filename,history)
-else:
-    print(history.shape)
-    print("Oops!  History is not in good shape. Check data or code...")
+#Save only 3 point averages otherwise you need 75G per patient
+smooth = np.zeros((data.shape[0],data.shape[1],3))
+for counter in range(steps):
+    newdata = W.dot(data)
+    data = data+newdata
+    if counter == 4:
+        smooth[:,:,0] = data
+    if counter == 49:
+        smooth[:,:,1] = data
+    if counter == 499:
+        smooth[:,:,2] = data
+
+print("Saving history...")
+filename = str(morphology)+'.walkhistory'    
+np.save(filename,smooth)
 
--- SG/heatmap_module/pipeline.serial.sh
+++ SG/heatmap_module/pipeline.serial.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-id=$1     # patient id
-stain=$2  #HE or DAPI
+id=$1     # .txt.gz file
+stain=$2  # HE or DAPI
 steps=$3  # radius of the subgraph for averaging features
 if [ -d /usr/local/share/anaconda3 ]; then
     path2anaconda=/usr/local/share/anaconda3/bin
@@ -12,17 +12,19 @@ if [ -d /home/garner1/miniconda3 ]; then
     echo The python executable directory is $path2anaconda
 fi
 
-#path2data=/media/garner1/hdd2/$stain/segmentation
-path2data=.
+path2data=/media/garner1/hdd2/$stain/segmentation
+#path2data=.
+echo The python data path is $path2data
 
-if [ -f $path2data/${id}__*.txt.gz ]; then
-    # echo Make Graph
-    # /usr/bin/time -v $path2anaconda/python3.7 pipe3.makeGraph.fromQuPath.py $path2data/${id}__*.txt.gz
+if [ -f ${id} ]; then
+    if [ ! -f ${id}.nn10.adj.npz ]; then
+	echo Make Graph
+	$path2anaconda/python3.7 pipe3.makeGraph.fromQuPath.py ${id}
+    fi
     echo Make average windows
-    #ls -l $path2data/${id}__*{.txt.gz,.adj.npz,degree.gz,cc.gz}
-    /usr/bin/time -v $path2anaconda/python3.7 pipe4.walk.fromQuPath.py $path2data/${id}__*{.txt.gz,.adj.npz,degree.gz,cc.gz} $steps
-    echo Make heatmaps
-    /usr/bin/time -v parallel "$path2anaconda/python3.7 pipe5.drawHeatMap.fromQuPath.py $path2data/${id}__*{.npy,.txt.gz} 5 {} ${id} deciles linear noflip ./" ::: 1 10 100 1000
+    $path2anaconda/python3.7 pipe4.walk.fromQuPath.py ${id} ${id}{.nn10.adj.npz,.nn10.degree.gz,.nn10.cc.gz} $steps
+    # echo Make heatmaps
+    # parallel "$path2anaconda/python3.7 pipe5.drawHeatMap.fromQuPath.py $path2data/${id}__*{.npy,.txt.gz} 5 {} ${id} deciles linear noflip ./" ::: 1 10 100 1000
 else
     echo $path2data/${id}__*.txt.gz does not exist!
 fi
