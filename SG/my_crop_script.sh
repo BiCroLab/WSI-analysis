fa4f9d48ede9388e3ace1c8b7be717770a018369
garner1 <silvano.garnerone@gmail.com>
2020-03-02 09:38:51 +0100

    step set to 15000

--- SG/my_crop_script.sh
+++ SG/my_crop_script.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 
 file=$1
+step=15000
+overlap=0
+
 height=$(/usr/bin/tiffinfo $file | grep Length | awk '{print $NF}')
 width=$(/usr/bin/tiffinfo $file | grep Width | awk '{print $3}')
 
-xlist=$(seq 0 10000 $width)
-xlast=$(seq 0 10000 $width | tail -1)
+xlist=$(seq 0 $step $width)
+xlast=$(seq 0 $step $width | tail -1)
 
 for x0 in $xlist; do
     echo $x0
     if [[ $x0 == 0 ]]
     then
 	xstart=$x0
-	lwidth=10000
+	lwidth=$step
 	batch_crop $xstart 0 $lwidth $height $file
     fi
     if [[ $x0 > 0 ]] && [[ $x0 < $xlast ]]
     then
-	xstart=$(($x0 - 1000))
-	lwidth=11000	
+	xstart=$(($x0 - $overlap))
+	lwidth=$step	
 	batch_crop $xstart 0 $lwidth $height $file
     fi
     if [[ $x0 == $xlast ]]
     then
-	xstart=$(($x0 - 1000))
+	xstart=$(($x0 - $overlap))
 	lwidth=$(($width - $xstart))
 	batch_crop $xstart 0 $lwidth $height $file
     fi
