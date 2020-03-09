e7e325cc903dedd8ccd118ebc91db0b8fdacb2e9
garner1 <silvano.garnerone@gmail.com>
2020-03-09 10:42:42 +0100

    descriptor is now rotationally invariant; OPTICS used for clustering instead of DBSCAN

--- SG/heatmap_module/pipeline.woPCA.py
+++ SG/heatmap_module/pipeline.woPCA.py
@@ -6,16 +6,18 @@ import sys
 from scipy import sparse, linalg
 import os
 import seaborn as sns; sns.set()
+
+import matplotlib
+matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
 import matplotlib.pyplot as plt
 
 filename = sys.argv[1] # name of the morphology mesearements from qupath
 radius = int(sys.argv[2])   # for smoothing
 quantiles = int(sys.argv[3]) # for stratifing the projection
 threshold = int(sys.argv[4]) # min number of nodes in a subgraph
-colindex = int(sys.argv[5]) # column to use in the morphology file
 
 basename_graph = os.path.splitext(os.path.basename(filename))[0]
-basename_smooth = os.path.splitext(os.path.splitext(os.path.basename(filename))[0])[0]+'.r'+str(radius)+'.i'+str(colindex)
+basename_smooth = os.path.splitext(os.path.splitext(os.path.basename(filename))[0])[0]+'.r'+str(radius)
 if os.path.splitext(os.path.basename(filename))[1] == '.gz':
     basename = os.path.splitext(os.path.splitext(os.path.basename(filename))[0])[0]+'.r'+str(radius)+'.q'+str(quantiles)+'.t'+str(threshold)
 elif os.path.splitext(os.path.basename(filename))[1] == '.txt':
@@ -56,6 +58,7 @@ if os.path.exists(path):
     d = getdegree(G)
     cc = clusteringCoeff(A)
 print('Topological graph ready!')
+pos2norm = np.linalg.norm(pos,axis=1).reshape((pos.shape[0],1)) # the modulus of the position vector
 
 ####################################################################################################
 # Select the morphological features,
@@ -64,7 +67,6 @@ print('Topological graph ready!')
 
 # Features list =  Nucleus:_Area   Nucleus:_Perimeter      Nucleus:_Circularity    Nucleus:_Eccentricity   Nucleus:_Hematoxylin_OD_mean    Nucleus:_Hematoxylin_OD_sum
 morphology = np.loadtxt(filename, delimiter="\t", skiprows=True, usecols=(7,8,9,12,13,14)).reshape((A.shape[0],6))
-# morphology = np.loadtxt(filename, delimiter="\t", skiprows=True, usecols=(colindex)).reshape((A.shape[0],1))
 
 ####################################################################################################
 # Smooth the morphology
@@ -81,7 +83,7 @@ else:
 print('Done!')
 
 ####################################################################################################
-# Project principal components back to real space
+# Stratify morphologies
 ###################################################################################################
 print('Project back to real space')
 import pandas as pd
@@ -89,48 +91,65 @@ import pandas as pd
 node_colors = np.zeros(morphology.shape)
 for f in range(morphology.shape[1]):
     node_colors[:,f] = pd.qcut(morphology_smooth[:,f].ravel(), quantiles, labels=False)
-node_color = node_colors[:,colindex-7]
 
 print('Done!')
 
 ####################################################################################################
 # Get subgraphs from multi-features
 ###################################################################################################
+print('Get the subgraphs')
+outfile_subgraphs = os.path.join(dirname, basename)+'.subgraphs.npy'
+outfile_node_list = os.path.join(dirname, basename)+'.node_list.npy'
+if os.path.exists(outfile_subgraphs) and os.path.exists(outfile_node_list):
+    subgraphs = np.load(outfile_subgraphs,allow_pickle=True)
+    node_list = np.load(outfile_node_list,allow_pickle=True)
+else:
+    subgraphs, node_list = get_subgraphs(G,threshold,quantiles,node_colors) # subgraph above threshold and flatten list of nodes
+    np.save(outfile_subgraphs,subgraphs)
+    np.save(outfile_node_list,node_list)
+print('Done!')
 
-# subgraphs = get_subgraphs(G,threshold,quantiles,node_colors)
-# print(len(subgraphs))
+U = G.subgraph(node_list)
+graphs = [g for g in list(nx.connected_component_subgraphs(U))] # this threshold check might be redundant
+# print('The list of nodes per graph is:')
+# print([g.number_of_nodes() for g in graphs])
+
 ####################################################################################################
 # Partition the graph and generate the covariance descriptors
 ###################################################################################################
 print('Generate the covariance descriptor')
-outfile_covd = os.path.join(dirname, basename)+'.i'+str(colindex)+'.covd.npy'
-outfile_graph2covd = os.path.join(dirname, basename)+'.i'+str(colindex)+'.graph2covd.npy'
+outfile_covd = os.path.join(dirname, basename)+'.covd.npy'
+outfile_graph2covd = os.path.join(dirname, basename)+'.graph2covd.npy'
 if os.path.exists(outfile_covd) and os.path.exists(outfile_graph2covd):
     covdata = np.load(outfile_covd,allow_pickle=True)
     graph2covd = np.load(outfile_graph2covd,allow_pickle=True)
 else:
-    features = np.loadtxt(filename, delimiter="\t", skiprows=True, usecols=(5,6,7,8,9,12,13,14)).reshape((A.shape[0],8)) #including X,Y
-    covdata, graph2covd = covd(features,G,threshold,quantiles,node_color)
+    features = np.hstack((pos2norm,morphology_smooth)) # this is rotational invariant
+    covdata, graph2covd = covd_multifeature(features,G,subgraphs) # get list of cov matrices and a list of nodes per matrix
     np.save(outfile_covd,covdata)
     np.save(outfile_graph2covd,graph2covd)
 print('Done!')
+
 ####################################################################################################
 # Cluster the covariance descriptors
+# TO DO: need to adjust clustering parameters to the statistics of the subgraph partitioning and try OPTICS instead of HDBSCAN
 ###################################################################################################
 print('Cluster the descriptors')
 import umap
 import hdbscan
 import sklearn.cluster as cluster
+from sklearn.cluster import OPTICS
 from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
 
-flat_covdata = [item for sublist in covdata for item in sublist] #flatten the list of lists
-logvec = [linalg.logm(m).reshape((1,16*16))  for m in flat_covdata] #calculate the logm and vectorize
-X = np.vstack(logvec) #create the array of covd data
+logvec = [linalg.logm(m).reshape((1,covdata[0].shape[0]*covdata[0].shape[1]))  for m in covdata] #calculate the logm and vectorize
+X = np.vstack(logvec) #create the array of vectorized covd data
 
-standard_embedding = umap.UMAP(random_state=42).fit_transform(X)
-clusterable_embedding = umap.UMAP(n_neighbors=10,min_dist=0.0,n_components=2,random_state=42).fit_transform(X)
+standard_embedding = umap.UMAP(random_state=42).fit_transform(X) # this is used to plot
+clusterable_embedding = umap.UMAP(n_neighbors=30,min_dist=0.0,n_components=4,random_state=42).fit_transform(X) # this is used to identify clusters
 
-labels = hdbscan.HDBSCAN(min_samples=10,min_cluster_size=20).fit_predict(clusterable_embedding)
+# labels = hdbscan.HDBSCAN(min_samples=25,min_cluster_size=50).fit_predict(clusterable_embedding)
+labels = OPTICS().fit(clusterable_embedding).labels_
+
 clustered = (labels >= 0)
 plt.scatter(standard_embedding[~clustered, 0],
             standard_embedding[~clustered, 1],
@@ -141,45 +160,40 @@ plt.scatter(standard_embedding[clustered, 0],
             standard_embedding[clustered, 1],
             c=labels[clustered],
             s=0.1,
-            cmap='Spectral');
+            cmap='viridis');
 
-outfile = os.path.join(dirname, basename_smooth)+'.covd-clustering.png'
+outfile = os.path.join(dirname, basename)+'.covd-clustering.png'
 plt.savefig(outfile) # save as png
 plt.close()
 print('Done')
-# ####################################################################################################
-# # Color nodes by labels
-# ###################################################################################################
-# print('Color the graph by descriptor cluster')
-# ind = 0
-# node_cluster_color = np.zeros(node_color.shape)
-# for item in graph2covd: # for each subgraph
-#     node_cluster_color[list(item[0][1])] = labels[ind] # update node_color with cluster labels for each node in the subgraph
-#     ind += 1
+####################################################################################################
+# Color nodes by labels
+###################################################################################################
 
-# clustered = (node_cluster_color >= 0)
-# clustered_nodes = np.asarray(list(G.nodes))[clustered] 
-# clustered_nodes_color = node_cluster_color[clustered] 
-# subG = G.subgraph(clustered_nodes)
+print('Color the graph by descriptor cluster')
 
-# sns.set(style='white', rc={'figure.figsize':(50,50)})
-# nx.draw_networkx_nodes(subG, pos, alpha=0.5,node_color=clustered_nodes_color, node_size=1,cmap='viridis')
+node_cluster_color = -1.0*np.ones(A.shape[0]) #check
+ind = 0
+for nodes in graph2covd: # for each subgraph
+    node_cluster_color[nodes] = labels[ind] # update node_color with cluster labels for each node in the subgraph
+    ind += 1
 
-# plt.margins(0,0)
-# plt.gca().xaxis.set_major_locator(plt.NullLocator())
-# plt.gca().yaxis.set_major_locator(plt.NullLocator())
+clustered = (node_cluster_color >= 0) # bolean array with true for clustered nodes and false for the rest
+print(str(sum(clustered))+' nodes clustered of '+str(clustered.shape[0])+' in total')
+clustered_nodes = np.asarray(list(G.nodes))[clustered] 
+clustered_nodes_color = node_cluster_color[clustered] 
+subG = G.subgraph(clustered_nodes)
 
-# plt.axis('off')
-# outfile = os.path.join(dirname, basename)+'.node-clustering.png'
-# plt.savefig(outfile, dpi=100,bbox_inches = 'tight', pad_inches = 0.5) # save as png
-# plt.close()
-# print('Done!')
-# ###################################################################################################
-# # Prepare a new data set based on a given descriptor cluster
-# ###################################################################################################
-# clustered = (node_cluster_color >= 0)
-# for cluster in set(node_cluster_color):
-#     clustered = (node_cluster_color == cluster)
-#     outfile = os.path.join(dirname, basename)+'.c'+str(int(cluster))
-#     np.save(outfile,clustered)
+sns.set(style='white', rc={'figure.figsize':(50,50)})
+nx.draw_networkx_nodes(subG, pos, alpha=0.5,node_color=clustered_nodes_color, node_size=1,cmap='viridis')
+
+plt.margins(0,0)
+plt.gca().xaxis.set_major_locator(plt.NullLocator())
+plt.gca().yaxis.set_major_locator(plt.NullLocator())
+
+plt.axis('off')
+outfile = os.path.join(dirname, basename)+'.node-clustering.png'
+plt.savefig(outfile, dpi=100,bbox_inches = 'tight', pad_inches = 0.5) # save as png
+plt.close()
+print('Done!')
 
