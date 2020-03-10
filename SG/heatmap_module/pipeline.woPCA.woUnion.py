c4cffc9c025dfecdd10b9917cef0a970a5c110b8
garner1 <silvano.garnerone@gmail.com>
2020-03-10 10:41:30 +0100

    morphology features reduced and clustering simplified

--- SG/heatmap_module/pipeline.woPCA.woUnion.py
+++ SG/heatmap_module/pipeline.woPCA.woUnion.py
@@ -63,11 +63,12 @@ pos2norm = np.linalg.norm(pos,axis=1).reshape((pos.shape[0],1)) # the modulus of
 
 ####################################################################################################
 # Select the morphological features,
-# normalize the feature matrix
+# and set the min number of nodes per subgraph
 ###################################################################################################
 
 # Features list =  Nucleus:_Area   Nucleus:_Perimeter      Nucleus:_Circularity    Nucleus:_Eccentricity   Nucleus:_Hematoxylin_OD_mean    Nucleus:_Hematoxylin_OD_sum
-morphology = np.loadtxt(filename, delimiter="\t", skiprows=True, usecols=(7,8,9,12,13,14)).reshape((A.shape[0],6))
+# morphology = np.loadtxt(filename, delimiter="\t", skiprows=True, usecols=(7,8,9,12,13,14)).reshape((A.shape[0],6))
+morphology = np.loadtxt(filename, delimiter="\t", skiprows=True, usecols=(7,9,12,13)).reshape((A.shape[0],4)) 
 threshold = max(radius,(morphology.shape[1]+4)*2) # set the min subgraph size based on the dim of the feature matrix
 
 ####################################################################################################
@@ -82,7 +83,6 @@ else:
     morphology_smooth = smoothing(A, morphology, radius)
     np.save(outfile, morphology_smooth)
 
-print('Done!')
 
 ####################################################################################################
 # Stratify morphologies
@@ -94,27 +94,30 @@ node_quantiles = np.zeros(morphology.shape)
 for f in range(morphology.shape[1]):
     node_quantiles[:,f] = pd.qcut(morphology_smooth[:,f].ravel(), quantiles, labels=False)
 
-print('Done!')
-
 ####################################################################################################
 # Get subgraphs from multi-features as the connected components in each quantile
 ###################################################################################################
 print('Get the subgraphs')
 outfile_subgraphs = os.path.join(dirname, basename)+'.subgraphs.npy'
-if os.path.exists(outfile_subgraphs):
+outfile_nodes = os.path.join(dirname, basename)+'.subgraphs_nodes.npy'
+if os.path.exists(outfile_subgraphs) and os.path.exists(outfile_nodes):
     print('... loading the subgraphs ...')
     subgraphs = np.load(outfile_subgraphs,allow_pickle=True)
+    unique_nodes = np.load(outfile_nodes,allow_pickle=True)
 else:
     print('... creating the subgraphs ...')
     # subgraph above threshold with thresh large enough to filter part of the nodes otherwise the union graph will be the entire graph
     subgraphs, unique_nodes = get_subgraphs(G,threshold,quantiles,node_quantiles)
-    
     np.save(outfile_subgraphs,subgraphs)
     np.save(outfile_nodes,unique_nodes)
-
+    
 print('There are '+str(len(subgraphs))+' subgraphs ')
+print('There are '+str(len(unique_nodes))+' unique nodes in the subgraphs ')
 
-print('Done')
+# print('...Generating the union graph...')
+# U = G.subgraph(unique_nodes)
+# graphs = [g for g in list(nx.connected_component_subgraphs(U)) if g.number_of_nodes()>=threshold] # this threshold check might be redundant
+# print('There are '+str(len(graphs))+' subgraphs in the union graph')
 
 ####################################################################################################
 # Generate the covariance descriptors
@@ -147,27 +150,44 @@ import sklearn.cluster as cluster
 from sklearn.cluster import OPTICS
 from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
 
-logvec = [linalg.logm(m).reshape((1,covdata[0].shape[0]*covdata[0].shape[1]))  for m in covdata] #calculate the logm and vectorize
-X = np.vstack(logvec) #create the array of vectorized covd data
+print('...prepare the data...')
+outfile_logvec = os.path.join(dirname, basename)+'.logvec.npy'
+if os.path.exists(outfile_logvec):
+    print('...load the logvec dataset...')
+    X = np.load(outfile_logvec,allow_pickle=True)
+else:
+    print('...create the logvec dataset...')
+    logvec = [linalg.logm(m).reshape((1,covdata[0].shape[0]*covdata[0].shape[1]))  for m in covdata] #calculate the logm and vectorize
+    X = np.vstack(logvec) #create the array of vectorized covd data
+    np.save(outfile_logvec,X)
+    
+outfile_clusterable_embedding = os.path.join(dirname, basename)+'.clusterable_embedding.npy'
+if os.path.exists(outfile_clusterable_embedding):
+    print('...load the clusterable embedding...')
+    clusterable_embedding = np.load(outfile_clusterable_embedding,allow_pickle=True)
+else:
+    print('...create the clusterable embedding...')
+    # clusterable_embedding = umap.UMAP(n_neighbors=50,min_dist=0.0,n_components=2,random_state=42).fit_transform(X) # this is used to identify clusters
+    clusterable_embedding = umap.UMAP(min_dist=0.0,n_components=2,random_state=42).fit_transform(X) # this is used to identify clusters
+    np.save(outfile_clusterable_embedding,clusterable_embedding)
 
-standard_embedding = umap.UMAP(random_state=42).fit_transform(X) # this is used to plot
-clusterable_embedding = umap.UMAP(n_neighbors=30,min_dist=0.0,n_components=2,random_state=42).fit_transform(X) # this is used to identify clusters
-
+print('...cluster the descriptors...')
 # labels = hdbscan.HDBSCAN(min_samples=25,min_cluster_size=50).fit_predict(clusterable_embedding)
 labels = OPTICS().fit(clusterable_embedding).labels_ # cluster label vector of covmatrices
 
+print('...plot the clusters...')
 clusteredD = (labels >= 0)
 non_clusteredD = (labels < 0)
 print(str(sum(clusteredD))+' descriptors clustered of '+str(labels.shape[0])+' in total')
 print(str(sum(non_clusteredD))+' descriptors NOT clustered of '+str(labels.shape[0])+' in total')
 
-plt.scatter(standard_embedding[~clusteredD, 0],
-            standard_embedding[~clusteredD, 1],
-            c='k',#(0.5, 0.5, 0.5),
-            s=0.1,
-            alpha=0.5)
-plt.scatter(standard_embedding[clusteredD, 0],
-            standard_embedding[clusteredD, 1],
+# plt.scatter(standard_embedding[~clusteredD, 0],
+#             standard_embedding[~clusteredD, 1],
+#             c='k',#(0.5, 0.5, 0.5),
+#             s=0.1,
+#             alpha=0.5)
+plt.scatter(clusterable_embedding[clusteredD, 0],
+            clusterable_embedding[clusteredD, 1],
             c=labels[clusteredD],
             s=0.1,
             cmap='viridis');
@@ -175,7 +195,6 @@ plt.scatter(standard_embedding[clusteredD, 0],
 outfile = os.path.join(dirname, basename)+'.covd-clustering.png'
 plt.savefig(outfile) # save as png
 plt.close()
-print('Done')
 
 ####################################################################################################
 # Color nodes by labels
@@ -184,7 +203,7 @@ print('Done')
 print('Color the graph by descriptor cluster')
 
 node_cluster_color = -1.0*np.ones(A.shape[0]) #check
-ind = 0
+ind = 0                                     # this is the subgraph label
 for nodes in graph2covd:                    # for each subgraph and corresponding covariance matrix
     node_cluster_color[nodes] = labels[ind] # update node_color with cluster labels for each node in the subgraph
     ind += 1
@@ -208,9 +227,9 @@ plt.gca().yaxis.set_major_locator(plt.NullLocator())
 
 plt.axis('off')
 outfile = os.path.join(dirname, basename)+'.node-clustering.png'
-plt.savefig(outfile, dpi=100,bbox_inches = 'tight', pad_inches = 0.5) # save as png
+plt.savefig(outfile, dpi=100) # save as png
+# plt.savefig(outfile, dpi=100,bbox_inches = 'tight', pad_inches = 0.5) # save as png
 plt.close()
-print('Done!')
 
 ####################################################################################################
 # Color nodes not clustered before
@@ -223,10 +242,10 @@ graphs = [g for g in list(nx.connected_component_subgraphs(subGnot)) if g.number
 
 print('Determine covariance matrix of the non clustered connected components')
 covdata, graph2covd = covd_multifeature(features,G,graphs) 
-
 logvec = [linalg.logm(m).reshape((1,covdata[0].shape[0]*covdata[0].shape[1])) for m in covdata] #calculate the logm and vectorize
 X_nonclustered = np.vstack(logvec) #create the array of vectorized covd data
 
+print('Cluster the other connected components by finding the min distance to the clustered subgraphs')
 from scipy import spatial
 reference = X[clusteredD,:]
 tree = spatial.KDTree(reference)
@@ -244,6 +263,7 @@ plt.gca().yaxis.set_major_locator(plt.NullLocator())
 
 plt.axis('off')
 outfile = os.path.join(dirname, basename)+'.all-node-clustering.png'
-plt.savefig(outfile, dpi=100,bbox_inches = 'tight', pad_inches = 0.5) # save as png
+plt.savefig(outfile, dpi=100) # save as png
+# plt.savefig(outfile, dpi=100,bbox_inches = 'tight', pad_inches = 0.5) # save as png
 plt.close()
 
