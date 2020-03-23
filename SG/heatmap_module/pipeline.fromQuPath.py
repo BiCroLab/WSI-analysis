823559a6452ad56bbf69b4d9f9cc81e107debf67
garner1 <silvano.garnerone@gmail.com>
2020-03-23 14:22:18 +0100

    pipeline.fromQuPath.new_test.py has become pipeline.fromQuPath.py

--- SG/heatmap_module/pipeline.fromQuPath.py
+++ SG/heatmap_module/pipeline.fromQuPath.py
@@ -5,6 +5,7 @@ from graviti import *
 import sys 
 from scipy import sparse, linalg
 import os
+import copy
 import seaborn as sns; sns.set()
 
 import matplotlib
@@ -13,12 +14,11 @@ import matplotlib.pyplot as plt
 
 filename = sys.argv[1] # name of the morphology mesearements from qupath
 radius = int(sys.argv[2])   # for smoothing
-quantiles = int(sys.argv[3]) # for stratifing the projection
 
 basename_graph = os.path.splitext(os.path.basename(filename))[0]
 basename_smooth = os.path.splitext(os.path.splitext(os.path.basename(filename))[0])[0]+'.r'+str(radius)
 if os.path.splitext(os.path.basename(filename))[1] == '.gz':
-    basename = os.path.splitext(os.path.splitext(os.path.basename(filename))[0])[0]+'.r'+str(radius)+'.q'+str(quantiles)
+    basename = os.path.splitext(os.path.splitext(os.path.basename(filename))[0])[0]+'.r'+str(radius)
 elif os.path.splitext(os.path.basename(filename))[1] == '.txt':
     basename = os.path.splitext(os.path.basename(filename))[0]
 dirname = os.path.dirname(filename)
@@ -60,8 +60,9 @@ pos2norm = np.linalg.norm(pos,axis=1).reshape((pos.shape[0],1)) # the modulus of
 # Select the morphological features,
 # and set the min number of nodes per subgraph
 ###################################################################################################
-
+print('Prepare the morphology array')
 # Features list =  Nucleus:_Area   Nucleus:_Perimeter      Nucleus:_Circularity    Nucleus:_Eccentricity   Nucleus:_Hematoxylin_OD_mean    Nucleus:_Hematoxylin_OD_sum
+from sklearn.preprocessing import normalize 
 morphology = np.loadtxt(filename, delimiter="\t", skiprows=True, usecols=(7,8,9,12,13,14)).reshape((A.shape[0],6))
 threshold = max(radius,(morphology.shape[1]+4)*2) # set the min subgraph size based on the dim of the feature matrix
 
@@ -78,107 +79,97 @@ else:
     np.save(outfile, morphology_smooth)
 
 ####################################################################################################
-# Stratify smoothed morphologies 
+# Reweight the graph
 ###################################################################################################
-print('Stratify morphologies')
-import pandas as pd
+print('Rescale graph weights by local morphology')
+ww = []
+morphology = normalize(morphology, norm='l1', axis=0) # normalize features
+morphology_smooth = normalize(morphology_smooth, norm='l1', axis=0) # normalize features
+GG = copy.deepcopy(G)
+for ijw in G.edges(data='weight'):
+    # !!!identical morphologies are discarded at the moment!!!
+    feature = np.asarray([ abs(morphology[ijw[0],f]-morphology[ijw[1],f]) for f in range(morphology.shape[1]) ]) # array of morphology features 
 
-node_quantiles = np.zeros(morphology.shape)
-for f in range(morphology.shape[1]):
-    node_quantiles[:,f] = pd.qcut(morphology_smooth[:,f].ravel(), quantiles, labels=False)
+    GG[ijw[0]][ijw[1]]['weight'] = ijw[2]/np.sum(feature) # the new graph weights
 
 ####################################################################################################
-# Get subgraphs from multi-features as the connected components in each quantile
+# Community detection
 ###################################################################################################
-print('Get the subgraphs')
-outfile_subgraphs = os.path.join(dirname, basename)+'.subgraphs.npy'
-outfile_nodes = os.path.join(dirname, basename)+'.subgraphs_nodes.npy'
-if os.path.exists(outfile_subgraphs) and os.path.exists(outfile_nodes):
-    print('... loading the subgraphs ...')
-    subgraphs = np.load(outfile_subgraphs,allow_pickle=True)
-    unique_nodes = np.load(outfile_nodes,allow_pickle=True)
+print('Find the communities')
+from cdlib import algorithms
+import igraph
+
+CC = list(nx.connected_component_subgraphs(GG))
+
+outfile = os.path.join(dirname, basename)+'.communities'
+if os.path.exists(outfile+'.npy'):
+    communities = np.load(outfile+'.npy',allow_pickle=True)
 else:
-    print('... creating the subgraphs ...')
-    # subgraph above threshold with thresh large enough to filter part of the nodes otherwise the union graph will be the entire graph
-    subgraphs, unique_nodes = get_subgraphs(G,threshold,quantiles,node_quantiles) #the subgraphs might be overlapping, between different features
-    np.save(outfile_subgraphs,subgraphs)
-    np.save(outfile_nodes,unique_nodes)
+    communities = []
+    for graph in CC:
+        file_edgelist = str(outfile)+'.edge_list.csv'
+        nx.write_edgelist(graph,file_edgelist,data=['weight']) # writhe the edge list 
+        g = igraph.Graph.Read_Ncol(file_edgelist, directed = False, weights = True)
+        weights = g.es['weight']
+        coms = algorithms.leiden(g,weights='weight')
+        communities.append(coms.communities)
+    np.save(outfile, communities)
 
-outfile_subgraph_size = os.path.join(dirname, basename)+'.subgraph_size.npy'
-np.save(outfile_subgraph_size,[len(g) for g in subgraphs])
-print('There are '+str(len(subgraphs))+' subgraphs ')
-print('There are '+str(len(unique_nodes))+' unique nodes in the subgraphs ')
+bigcommunities = [sg for g in communities for sg in g if len(sg) > threshold] # flatten list of communities
+outfile = os.path.join(dirname, basename)+'.bigcommunities'
+np.save(outfile, bigcommunities)
+print('There are '+str(len(bigcommunities))+' big communities and '+str(len([sg for g in communities for sg in g]))+' communities in total')
 
 ####################################################################################################
-# Find the largest subgraph of each node
+# Generate the covariance descriptors
+# this can be done with respect to raw features or smoothed ones
 ###################################################################################################
-list_size = [len(g) for g in subgraphs]
-outfile_n2i = os.path.join(dirname, basename)+'.node2index.npy'
-n2i = {}
-for n in unique_nodes:
-    subg = [ g for g in subgraphs if ((n in g) and (len(g) <= 10000)) ] #find the subgraphs containing a given n
-    max_subg = max((len(g)) for g in subg) # determine the largest subgraph of those containing n
-    index = list_size.index(max_subg) # the index location in subgraphs of the largest one
-    n2i[n] = index
-np.save(outfile_n2i,n2i)
-# ####################################################################################################
-# # Generate the covariance descriptors
-# # this can be done with respect to raw features or smoothed ones
-# ###################################################################################################
-# subgraph = [g for g in subgraphs if len(g) <= 10000]
-# print('Generate the covariance descriptor')
-# features = np.hstack((pos2norm,morphology_smooth))            # this is rotational invariant
+print('Generate the covariance descriptor')
+features = np.hstack((pos2norm,morphology))            # this is rotational invariant
 
-# print('Generate the covariance descriptor')
-# features = np.hstack((pos2norm,morphology_smooth))            # this is rotational invariant
+outfile_covd = os.path.join(dirname, basename)+'.covd.npy'
+if os.path.exists(outfile_covd):
+    print('... loading the descriptors ...')
+    covdata = np.load(outfile_covd,allow_pickle=True)
+else:
+    print('... creating the descriptors ...')
+    covdata = community_covd(features,G,bigcommunities) # get list of cov matrices and a list of nodes per matrix
+    np.save(outfile_covd,covdata)
 
-# outfile_covd = os.path.join(dirname, basename)+'.covd.npy'
-# outfile_graph2covd = os.path.join(dirname, basename)+'.graph2covd.npy'
-# if os.path.exists(outfile_covd) and os.path.exists(outfile_graph2covd):
-#     print('... loading the descriptors ...')
-#     covdata = np.load(outfile_covd,allow_pickle=True)
-#     graph2covd = np.load(outfile_graph2covd,allow_pickle=True)
-# else:
-#     print('... creating the descriptors ...')
-#     covdata, graph2covd = covd_multifeature(features,G,subgraphs) # get list of cov matrices and a list of nodes per matrix
-#     np.save(outfile_covd,covdata)
-#     np.save(outfile_graph2covd,graph2covd)
+print('There are '+str(len(covdata))+' covariance descriptors ')
 
-# print('There are '+str(len(covdata))+' covariance descriptors ')
+####################################################################################################
+# Cluster the covariance descriptors
+###################################################################################################
+print('Clustering the descriptors')
+import umap
+import hdbscan
+import sklearn.cluster as cluster
+from sklearn.cluster import OPTICS
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
 
-# # ####################################################################################################
-# # # Cluster the covariance descriptors
-# # ###################################################################################################
-# print('Clustering the descriptors')
-# import umap
-# import hdbscan
-# import sklearn.cluster as cluster
-# from sklearn.cluster import OPTICS
-# from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
-# from sklearn.cluster import AgglomerativeClustering
-# from sklearn.cluster import SpectralClustering
-
-# print('...prepare the data...')
-# outfile_logvec = os.path.join(dirname, basename)+'.logvec.npy'
-# if os.path.exists(outfile_logvec):
-#     print('...load the logvec dataset...')
-#     X = np.load(outfile_logvec,allow_pickle=True)
-# else:
-#     print('...create the logvec dataset...')
-#     logvec = [linalg.logm(m).reshape((1,covdata[0].shape[0]*covdata[0].shape[1]))  for m in covdata] #calculate the logm and vectorize
-#     X = np.vstack(logvec) #create the array of vectorized covd data
-#     np.save(outfile_logvec,X)
-    
-# outfile_clusterable_embedding = os.path.join(dirname, basename)+'.clusterable_embedding.npy'
-# if False:#os.path.exists(outfile_clusterable_embedding):
-#     print('...load the clusterable embedding...')
-#     clusterable_embedding = np.load(outfile_clusterable_embedding,allow_pickle=True)
-# else:
-#     print('...create the clusterable embedding...')
-#     # clusterable_embedding = umap.UMAP(min_dist=0.0,n_components=2,random_state=42).fit_transform(X) # this is used to identify clusters
-#     clusterable_embedding = umap.UMAP(min_dist=0.0,n_components=3,random_state=42).fit_transform(X) # this is used to identify clusters
-#     np.save(outfile_clusterable_embedding,clusterable_embedding)
-
+print('...prepare the data...')
+outfile_logvec = os.path.join(dirname, basename)+'.logvec.npy'
+if os.path.exists(outfile_logvec):
+    print('...load the logvec dataset...')
+    X = np.load(outfile_logvec,allow_pickle=True)
+else:
+    print('...create the logvec dataset...')
+    logvec = [linalg.logm(m).reshape((1,covdata[0].shape[0]*covdata[0].shape[1]))  for m in covdata] #calculate the logm and vectorize
+    X = np.vstack(logvec) #create the array of vectorized covd data
+    np.save(outfile_logvec,X)
+print('The vectorized covd array has shape '+str(X.shape))    
+outfile_clusterable_embedding = os.path.join(dirname, basename)+'.clusterable_embedding.npy'
+if os.path.exists(outfile_clusterable_embedding):
+    print('...load the clusterable embedding...')
+    clusterable_embedding = np.load(outfile_clusterable_embedding,allow_pickle=True)
+else:
+    print('...create the clusterable embedding...')
+    clusterable_embedding = umap.UMAP(min_dist=0.0,n_components=3,random_state=42).fit_transform(X) # this is used to identify clusters
+    np.save(outfile_clusterable_embedding,clusterable_embedding)
+print('The embedding has shape '+str(clusterable_embedding.shape))
 # # print('...cluster the descriptors...')
 # # labels = hdbscan.HDBSCAN(min_samples=50,min_cluster_size=100).fit_predict(clusterable_embedding)
 # labels = OPTICS(min_samples=50, xi=.01, min_cluster_size=.05).fit_predict(clusterable_embedding) # cluster label vector of covmatrices
@@ -290,4 +281,6 @@ np.save(outfile_n2i,n2i)
 # plt.savefig(outfile, dpi=100) # save as png
 # # plt.savefig(outfile, dpi=100,bbox_inches = 'tight', pad_inches = 0.5) # save as png
 # plt.close()
+
+
 
